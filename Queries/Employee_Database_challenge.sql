b3a0848b316051b5ca52e09710dd95fde4c71781
KyleCaulkins <90616094+KyleCaulkins@users.noreply.github.com>
2021-10-30 21:37:20 -0500

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,64 @@
+--Deliverable 1
+--Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+--Retrieve the title, from_date, and to_date columns from the Titles table.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles as t
+	ON (e.emp_no=t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (r.emp_no)
+	r.emp_no,
+	r.first_name,
+	r.last_name,
+	r.title
+INTO unique_titles
+FROM retirement_titles as r
+ORDER BY r.emp_no, r.to_date DESC;
+
+SELECT * FROM unique_titles;
+
+--Retiring titles
+SELECT COUNT(u.emp_no), u.title
+INTO retiring_titles
+FROM unique_titles as u
+GROUP BY u.title
+ORDER BY COUNT (u.emp_no) DESC;
+
+SELECT * FROM retiring_titles;
+
+--Deliverable 2
+--Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+--Retrieve the from_date and to_date columns from the Department Employee table.
+--Retrieve the title column from the Titles table.
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS DE
+	ON (e.emp_no=de.emp_no)
+INNER JOIN titles AS t
+	ON (e.emp_no=t.emp_no)
+	WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+		AND (de.to_date='9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
+
